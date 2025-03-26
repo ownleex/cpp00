38705bac18137fea520f5c9a301cee67dbfca269
ownleex <145456737+ownleex@users.noreply.github.com>
2025-03-27 00:24:15 +0100

    Mise à jour des messages d'affichage dans main.cpp et ajout de nouvelles lignes pour une meilleure lisibilité. Modification des horodatages dans .gitignore et Makefile.

--- cpp_01/ex02/src/main.cpp
+++ cpp_01/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 00:02:05 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/03/27 00:15:18 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/03/27 00:23:27 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 #include <string>
 
 int main() {
+    std::cout << std::endl;
+
     std::string brain = "HI THIS IS BRAIN";
     std::string* stringPTR = &brain;
     std::string& stringREF = brain;
     
-    std::cout << "Adresse mémoire de la variable string: " << &brain << std::endl;
-    std::cout << "Adresse mémoire contenue dans stringPTR: " << stringPTR << std::endl;
-    std::cout << "Adresse mémoire contenue dans stringREF: " << &stringREF << std::endl;
+    std::cout << "Memory address of the string variable:   " << &brain << std::endl;
+    std::cout << "Memory address held by stringPTR:        " << stringPTR << std::endl;
+    std::cout << "Memory address held by stringREF:        " << &stringREF << std::endl;
     
-    std::cout << "Valeur de la string: " << brain << std::endl;
-    std::cout << "Valeur pointée par stringPTR: " << *stringPTR << std::endl;
-    std::cout << "Valeur pointée par stringREF: " << stringREF << std::endl;
+    std::cout << std::endl;
+    
+    std::cout << "Value of the string:                     " << brain << std::endl;
+    std::cout << "Value pointed to by stringPTR:           " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF:           " << stringREF << std::endl;
+    
+    std::cout << std::endl;
     
     return 0;
 }
