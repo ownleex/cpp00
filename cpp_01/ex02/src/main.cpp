99752ab4e3506edf01bd906aad868636fd915fd3
ownleex <145456737+ownleex@users.noreply.github.com>
2025-03-27 00:18:14 +0100

    Ajout d'un nouveau fichier main.cpp dans le répertoire src et mise à jour du Makefile pour inclure la compilation de ce fichier.

--- cpp_01/ex02/src/main.cpp
+++ cpp_01/ex02/src/main.cpp
@@ -6,7 +6,25 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 00:02:05 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/03/27 00:02:06 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/03/27 00:15:18 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string brain = "HI THIS IS BRAIN";
+    std::string* stringPTR = &brain;
+    std::string& stringREF = brain;
+    
+    std::cout << "Adresse mémoire de la variable string: " << &brain << std::endl;
+    std::cout << "Adresse mémoire contenue dans stringPTR: " << stringPTR << std::endl;
+    std::cout << "Adresse mémoire contenue dans stringREF: " << &stringREF << std::endl;
+    
+    std::cout << "Valeur de la string: " << brain << std::endl;
+    std::cout << "Valeur pointée par stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Valeur pointée par stringREF: " << stringREF << std::endl;
+    
+    return 0;
+}
