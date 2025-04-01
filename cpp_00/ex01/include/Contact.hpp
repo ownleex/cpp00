c0dec7d82284c8dd6100c844593b301f7883bb9d
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-01 19:01:53 +0200

    Modification du constructeur de la classe Contact pour utiliser les setters, ajout de la gestion des attributs via les getters dans les méthodes d'affichage.

--- cpp_00/ex01/include/Contact.hpp
+++ cpp_00/ex01/include/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 02:10:34 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/01 18:09:05 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/01 19:00:52 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,16 @@ private:
 	std::string darkestSecret;
 
 public:
-	Contact() : firstName(""), lastName(""), nickname(""), phoneNumber(""), darkestSecret("") {}
+	Contact(const std::string& firstName = "", const std::string& lastName = "", 
+			const std::string& nickname = "", const std::string& phoneNumber = "",
+			const std::string& darkestSecret = "");
 
-	Contact(const std::string& firstName, const std::string& lastName, const std::string& nickname,
-			const std::string& phoneNumber, const std::string& darkestSecret);
-
-	// Getters
 	std::string getFirstName() const;
 	std::string getLastName() const;
 	std::string getNickname() const;
 	std::string getPhoneNumber() const;
 	std::string getDarkestSecret() const;
 
-	// Setters
 	void setFirstName(const std::string& name);
 	void setLastName(const std::string& name);
 	void setNickname(const std::string& nick);
--- cpp_00/ex01/src/Contact.cpp
+++ cpp_00/ex01/src/Contact.cpp
@@ -6,17 +6,15 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 02:10:38 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/01 18:08:39 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/01 19:01:35 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Contact.hpp"
 #include <iomanip>
 
-// Constructeur modifié pour utiliser les setters au lieu d'initialiser directement
 Contact::Contact(const std::string& firstName, const std::string& lastName, const std::string& nickname,
 				 const std::string& phoneNumber, const std::string& darkestSecret) {
-    // Utilisation des setters pour initialiser les attributs
     setFirstName(firstName);
     setLastName(lastName);
     setNickname(nickname);
@@ -24,14 +22,12 @@ Contact::Contact(const std::string& firstName, const std::string& lastName, cons
     setDarkestSecret(darkestSecret);
 }
 
-// Implémentation des getters
 std::string Contact::getFirstName() const { return firstName; }
 std::string Contact::getLastName() const { return lastName; }
 std::string Contact::getNickname() const { return nickname; }
 std::string Contact::getPhoneNumber() const { return phoneNumber; }
 std::string Contact::getDarkestSecret() const { return darkestSecret; }
 
-// Implémentation des setters
 void Contact::setFirstName(const std::string& name) { firstName = name; }
 void Contact::setLastName(const std::string& name) { lastName = name; }
 void Contact::setNickname(const std::string& nick) { nickname = nick; }
@@ -39,14 +35,12 @@ void Contact::setPhoneNumber(const std::string& number) { phoneNumber = number; 
 void Contact::setDarkestSecret(const std::string& secret) { darkestSecret = secret; }
 
 void Contact::displayContactPreview() const {
-	// Utilisation des getters au lieu d'accéder directement aux attributs
 	std::cout << std::setw(10) << (getFirstName().size() > 10 ? getFirstName().substr(0, 9) + "." : getFirstName()) << '|'
 			  << std::setw(10) << (getLastName().size() > 10 ? getLastName().substr(0, 9) + "." : getLastName()) << '|'
 			  << std::setw(10) << (getNickname().size() > 10 ? getNickname().substr(0, 9) + "." : getNickname()) << '|' << std::endl;
 }
 
 void Contact::displayContactDetails() const {
-	// Utilisation des getters au lieu d'accéder directement aux attributs
 	std::cout << "First Name: " << getFirstName() << std::endl;
 	std::cout << "Last Name: " << getLastName() << std::endl;
 	std::cout << "Nickname: " << getNickname() << std::endl;
