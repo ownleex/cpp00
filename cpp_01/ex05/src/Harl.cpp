7c0c70b74887f5e4fbf0e20da34935ac9e630461
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-04 01:10:20 +0200

    Amélioration des messages de débogage dans Harl.cpp : fusion des lignes pour une meilleure lisibilité.

--- cpp_01/ex05/src/Harl.cpp
+++ cpp_01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 01:06:38 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/03/27 01:12:07 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/04 00:45:57 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,17 @@ Harl::~Harl() {
 
 void Harl::debug(void) {
     std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
-    std::cout << "I really do!" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void) {
     std::cout << "[ INFO ]" << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
-    std::cout << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) {
     std::cout << "[ WARNING ]" << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
-    std::cout << "I've been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
