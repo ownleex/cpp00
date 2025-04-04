00a92f1091549c34b94ab1e21e14f222d45efd6c
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-04 20:51:11 +0200

    Mise à jour des commentaires et formatage du code dans la classe Harl

--- cpp_01/ex05/src/Harl.cpp
+++ cpp_01/ex05/src/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 01:06:38 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/04 00:45:57 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/04 20:51:02 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,37 @@ Harl::~Harl() {
 }
 
 void Harl::debug(void) {
-    std::cout << "[ DEBUG ]" << std::endl;
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(void) {
-    std::cout << "[ INFO ]" << std::endl;
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(void) {
-    std::cout << "[ WARNING ]" << std::endl;
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(void) {
-    std::cout << "[ ERROR ]" << std::endl;
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level) {
-    // Array of member function pointers
-    typedef void (Harl::*MemberFunc)(void);
-    MemberFunc funcs[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-    
-    // Array of corresponding level strings
-    std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-    
-    // Find the matching level and call the corresponding function
-    for (int i = 0; i < 4; i++) {
-        if (level == levels[i]) {
-            (this->*funcs[i])();
-            return;
-        }
-    }
-    
-    // If no match is found
-    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	typedef void (Harl::*MemberFunc)(void);
+	MemberFunc funcs[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	for (int i = 0; i < 4; i++) {
+		if (level == levels[i]) {
+			(this->*funcs[i])();
+			return;
+		}
+	}
+	
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
