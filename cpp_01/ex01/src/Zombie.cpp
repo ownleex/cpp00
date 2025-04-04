59fe6c5435c65a1cb8b33b6c8b554bcdffc2e966
ownleex <145456737+ownleex@users.noreply.github.com>
2025-04-04 16:49:41 +0200

    Suppression du constructeur inutile dans Zombie.cpp

--- cpp_01/ex01/src/Zombie.cpp
+++ cpp_01/ex01/src/Zombie.cpp
@@ -6,17 +6,13 @@
 /*   By: ayarmaya <ayarmaya@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 23:03:13 by ayarmaya          #+#    #+#             */
-/*   Updated: 2025/04/03 17:40:43 by ayarmaya         ###   ########.fr       */
+/*   Updated: 2025/04/04 16:47:27 by ayarmaya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie() : name("") {
-	return;
-}
-
-Zombie::Zombie(std::string name) : name(name) {
 	return;
 }
 
